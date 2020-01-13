d35fc200d8b65d4353a18352a35baa29d98ce107
Daniel García <daniel@etereo.io>
2020-01-13 11:37:09 +0100

    Add basic integration tests using terratest

---
+++ tests/rules/release_spec_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/shell"
+)
+
+// Test suite for testing release of chart basic package
+func TestBasicChartRelease(t *testing.T) {
+	t.Parallel()
+
+	namespaceName := "test-nginx"
+	releaseName := "test-nginx"
+
+	// Setup the kubectl config and context. Here we choose to use the defaults, which is:
+	// - HOME/.kube/config for the kubectl config file
+	// - Current context of the kubectl config file
+	// - Random namespace
+	options := k8s.NewKubectlOptions("", "", namespaceName)
+
+	k8s.CreateNamespace(t, options, namespaceName)
+
+	shell.RunCommand(t, Command {
+		"bazel",
+		["run", "//tests/charts/nginx:nginx_helm_release"]
+	})
+
+	defer helm.Delete(t, options, releaseName, true)
+
+	defer k8s.DeleteNamespace(t, options, namespaceName)
+
+	pod := k8s.GetPod(t, options, "test-nginx")
+	require.Equal(t, pod.Name, "test-nginx")
+}
