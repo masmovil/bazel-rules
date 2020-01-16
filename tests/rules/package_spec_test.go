41d305f0946defff85412cf94029ca8249682a0d
Daniel García <daniel@etereo.io>
2020-01-16 15:35:29 +0100

    Add integration tests for make version var in package chart rule

--- tests/rules/package_spec_test.go
+++ tests/rules/package_spec_test.go
@@ -64,3 +64,55 @@ func TestChartPackageImageTagMakeVar(t *testing.T) {
 	require.Equal(t, deployment.Spec.Template.Spec.Containers[0].Image, image_repository+":"+imageTag)
 
 }
+
+func TestChartPackageChartVersionMakeVar(t *testing.T) {
+	t.Parallel()
+
+	chartVersion := "0.0.2"
+	chartTarPackagePath := "bazel-bin/tests/charts/nginx/nginx-" + chartVersion + ".tgz"
+	chartPackageRootPath := "nginx"
+	relativeChartPackageRootPath := "../../" + chartPackageRootPath
+
+	shell.RunCommand(t, shell.Command{
+		Command:           "bazel",
+		Args:              []string{"build", "//tests/charts/nginx:nginx_chart_make_version", "--define", "TEST_VERSION=" + chartVersion},
+		WorkingDir:        ".",
+		Env:               map[string]string{},
+		OutputMaxLineSize: 1024,
+	})
+
+	shell.RunCommand(t, shell.Command{
+		Command:           "tar",
+		Args:              []string{"-xzf", chartTarPackagePath},
+		WorkingDir:        "../..",
+		Env:               map[string]string{},
+		OutputMaxLineSize: 1024,
+	})
+
+	defer shell.RunCommand(t, shell.Command{
+		Command:           "rm",
+		Args:              []string{"-f", chartTarPackagePath},
+		WorkingDir:        "../..",
+		Env:               map[string]string{},
+		OutputMaxLineSize: 1024,
+	})
+
+	defer shell.RunCommand(t, shell.Command{
+		Command:           "rm",
+		Args:              []string{"-rf", chartPackageRootPath},
+		WorkingDir:        "../..",
+		Env:               map[string]string{},
+		OutputMaxLineSize: 1024,
+	})
+
+	output := helm.RenderTemplate(t, &helm.Options{
+		ValuesFiles: []string{
+			relativeChartPackageRootPath + "/values.yaml",
+		},
+	}, relativeChartPackageRootPath, "nginx", []string{"templates/deployment.yaml"})
+
+	var deployment appsv1.Deployment
+	helm.UnmarshalK8SYaml(t, output, &deployment)
+
+	require.Equal(t, deployment.ObjectMeta.Labels["version"], chartVersion)
+}
