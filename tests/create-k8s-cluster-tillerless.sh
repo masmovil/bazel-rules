1d250b4ffd28836d3204ce090e71d0ee17e8b2f9
Daniel García <daniel@etereo.io>
2020-03-24 18:23:45 +0100

    Coding integration testing of helm push rule using chart museum

--- tests/create-k8s-cluster-tillerless.sh
+++ tests/create-k8s-cluster-tillerless.sh
@@ -1,1 +1,3 @@
 kind create cluster --name bazel-tillerless
+kubectl create namespace system-chartmuseum
+helm install chartmuseum --namespace system-chartmuseum -f tests/resources/chartmuseum/values.yaml stable/chartmuseum
