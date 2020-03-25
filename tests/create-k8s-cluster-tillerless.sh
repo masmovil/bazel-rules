529a58b1a1472a9866d924a2351bf704140de4e7
Daniel García <daniel@etereo.io>
2020-03-25 10:30:47 +0100

    Update helm3 repo before installing chartmuseum

--- tests/create-k8s-cluster-tillerless.sh
+++ tests/create-k8s-cluster-tillerless.sh
@@ -1,3 +1,4 @@
 kind create cluster --name bazel-tillerless
 kubectl create namespace system-chartmuseum
+helm3 repo update
 helm3 install chartmuseum --namespace system-chartmuseum -f tests/resources/chartmuseum/values.yaml stable/chartmuseum
