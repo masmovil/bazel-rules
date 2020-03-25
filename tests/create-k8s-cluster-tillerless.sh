68a9e64a81935b5093461c8afbf188cd1ac70f36
Daniel García <daniel@etereo.io>
2020-03-25 10:15:25 +0100

    Install chartmuseum susing helm3

--- tests/create-k8s-cluster-tillerless.sh
+++ tests/create-k8s-cluster-tillerless.sh
@@ -1,3 +1,3 @@
 kind create cluster --name bazel-tillerless
 kubectl create namespace system-chartmuseum
-helm install chartmuseum --namespace system-chartmuseum -f tests/resources/chartmuseum/values.yaml stable/chartmuseum
+helm3 install chartmuseum --namespace system-chartmuseum -f tests/resources/chartmuseum/values.yaml stable/chartmuseum
