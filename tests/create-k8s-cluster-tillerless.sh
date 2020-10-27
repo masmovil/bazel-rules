c16923aba211ddde6e0643cb4155186556fd470f
Daniel García <daniel@etereo.io>
2020-10-27 15:03:14 +0100

    Update stabe helm repo

--- tests/create-k8s-cluster-tillerless.sh
+++ tests/create-k8s-cluster-tillerless.sh
@@ -1,5 +1,5 @@
 kind create cluster --name bazel-tillerless
 kubectl create namespace system-chartmuseum
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add stable https://charts.helm.sh/stable
 helm repo update
 helm install chartmuseum --namespace system-chartmuseum -f tests/resources/chartmuseum/values.yaml stable/chartmuseum
