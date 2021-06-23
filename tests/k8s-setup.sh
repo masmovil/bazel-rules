3620d537d2918078b7edb2ca756b548be74655f1
Daniel García <daniel.garciago@masmovil.com>
2021-06-23 16:42:07 +0200

    Move nmap after cluster creation

--- tests/k8s-setup.sh
+++ tests/k8s-setup.sh
@@ -1,6 +1,6 @@
-nmap -p- 127.0.0.1
 kind create cluster --config=./tests/cluster.yaml
 docker ps
+nmap -p- 127.0.0.1
 kubectl cluster-info --context kind-bazel-rules
 kubectl create namespace system-chartmuseum
 helm repo add stable https://charts.helm.sh/stable
