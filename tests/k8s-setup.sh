3c8a1be111a76fddb05915dfb6673f8abf33f1cf
Daniel García <daniel.garciago@masmovil.com>
2021-06-24 16:42:07 +0200

    Remove unnecessary scripts and cloudbuild/travis config

--- tests/k8s-setup.sh
+++
@@ -1,8 +0,0 @@
-kind create cluster --config=./tests/cluster.yaml
-docker ps
-nmap -p- 127.0.0.1
-kubectl cluster-info --context kind-bazel-rules
-kubectl create namespace system-chartmuseum
-helm repo add stable https://charts.helm.sh/stable
-helm repo update
-helm install chartmuseum --namespace system-chartmuseum -f tests/resources/chartmuseum/values.yaml stable/chartmuseum
